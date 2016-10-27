c45dd5de583a99a1bd6173730d509e5597a92a00
koksalb <koksalb@itu.edu.tr>
2016-10-27 23:59:29 +0300

    Connected IBM Bluemix App and Elephant SQL Service
    Added the connection information data: username password host and db
    
    Tested initialise_database on /initdb ONCE to initialize it
    
    Tested initialized database with a simple counter integer page for
    pagecounts

--- server.py
+++ server.py
@@ -1,17 +1,29 @@
-import datetime
+import json
 import os
-
+import ibm_db
+import re
+import psycopg2 as dbapi2
 from flask import Flask
+from flask import redirect
 from flask import render_template
-
+from flask.helpers import url_for
 
 app = Flask(__name__)
 
 
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
+    
 @app.route('/')
 def home_page():
-    now = datetime.datetime.now()
-    return render_template('home.html', current_time=now.ctime())
+    return render_template('home.html')
 
 @app.route('/samplecommit')
 def movies_page():
@@ -41,10 +53,51 @@ def add_tweet_page():
 def all_tweets_page():
     return render_template('allTweets.html')
 
+@app.route('/initdb')
+def initialize_database():
+    with dbapi2.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+
+        query = """DROP TABLE IF EXISTS COUNTER"""
+        cursor.execute(query)
+
+        query = """CREATE TABLE COUNTER (N INTEGER)"""
+        cursor.execute(query)
+
+        query = """INSERT INTO COUNTER (N) VALUES (0)"""
+        cursor.execute(query)
+
+        connection.commit()
+    return redirect(url_for('home_page'))
+
+@app.route('/count')
+def counter_page():
+    with dbapi2.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+
+        query = "UPDATE COUNTER SET N = N + 1"
+        cursor.execute(query)
+        connection.commit()
+
+        query = "SELECT N FROM COUNTER"
+        cursor.execute(query)
+        count = cursor.fetchone()[0]
+    return "This page was accessed %d times." % count
+
+
 if __name__ == '__main__':
+
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
     if VCAP_APP_PORT is not None:
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='hknbgryr' password='Yte_gcTO9oEeCFVM7dKv53djw8VnaJwP'
+                               host='jumbo.db.elephantsql.com' port=5432 dbname='hknbgryr'"""
+   
     app.run(host='0.0.0.0', port=port, debug=debug)
+
