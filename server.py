211f35b51a33c785ecdda03e114ae2910309f025
maydin <squirt.winkle@gmail.com>
2016-12-16 23:48:10 +0200

    server.py seperated into multiple files

--- server.py
+++ server.py
@@ -24,6 +24,16 @@ def get_elephantsql_dsn(vcap_services):
              dbname='{}'""".format(user, password, host, port, dbname)
     return dsn
 
+def create_app():
+    global app
+    from messages import messages
+
+    app.register_blueprint(messages)
+    return app
+
+
+
+
 @app.route('/page_login')
 def home_page():
     return render_template('home.html')
@@ -51,79 +61,9 @@ def page_login():
                  return render_template('loginpage.html')
         elif request.method == 'GET':
             return render_template('loginpage.html')
-    
-
-@app.route('/messages')
-def messages_page():
-    with dbapi2.connect(app.config['dsn']) as connection:
-        cursor = connection.cursor()
-        cursor.execute("""SELECT messages.id,U1.NAME,U1.LASTNAME,U2.NAME, U2.LASTNAME, messages.message,messages.timestamp
-        FROM messages
-        INNER JOIN USERS AS U1
-        ON messages.fromuser=U1.ID
-        INNER JOIN USERS AS U2
-        ON messages.touser=U2.ID"""
-        )
-        messages_all = cursor.fetchall()
-        connection.commit()
-    return render_template('messages.html', messages = messages_all)
-
-@app.route('/messages/new', methods = ['POST', 'GET'])
-def new_message_page():
-    if request.method == 'GET':
-        with dbapi2.connect(app.config['dsn']) as connection:
-            cursor = connection.cursor()
-            cursor.execute("""SELECT ID, NAME, LASTNAME FROM users"""
-            )
-            to_send = cursor.fetchall()
-            connection.commit()
-
-        return render_template('new_message.html', tosend = to_send)
-    else:
 
 
-        frm = current_user;
-        to = request.form['to']
-        msg = request.form['message']
-        timestamp = datetime.now();
 
-        with dbapi2.connect(app.config['dsn']) as connection:
-            cursor = connection.cursor()
-            query = """INSERT INTO MESSAGES (FROMUSER, TOUSER, MESSAGE, TIMESTAMP) VALUES ('%s', '%s', '%s', %s)""" % (frm, to, msg, 0)
-            cursor.execute(query)
-            connection.commit()
-        return redirect('/messages')
-
-@app.route('/messages/update/<int:message_id>', methods = ['POST', 'GET'])
-def update_message_page(message_id):
-    if request.method == 'GET':
-        with dbapi2.connect(app.config['dsn']) as connection:
-            cursor = connection.cursor()
-            query = """SELECT U1.NAME, U1.LASTNAME, U2.NAME, U2.LASTNAME, messages.message FROM MESSAGES, USERS AS U1, USERS AS U2 WHERE (MESSAGES.ID=%s) AND (U1.ID=MESSAGES.FROMUSER) AND (U2.ID=MESSAGES.TOUSER)""" % (message_id)
-            cursor.execute(query)
-            message = cursor.fetchall()
-            connection.commit()
-
-        return render_template('update_message.html', messages = message)
-    else:
-        msg = request.form['message']
-        timestamp = datetime.now();
-
-        with dbapi2.connect(app.config['dsn']) as connection:
-            cursor = connection.cursor()
-            query = """UPDATE MESSAGES SET MESSAGE='%s', TIMESTAMP=%s WHERE ID=%s""" % (msg, 0, message_id)
-            cursor.execute(query)
-            connection.commit()
-        return redirect('/messages')
-
-@app.route('/messages/delete/<int:message_id>')
-def delete_message_page(message_id):
-    with dbapi2.connect(app.config['dsn']) as connection:
-        cursor = connection.cursor()
-        query = """DELETE FROM MESSAGES WHERE ID=%s""" % (message_id)
-        cursor.execute(query)
-        connection.commit()
-    return redirect('/messages')
 
 @app.route('/signup', methods = ['POST', 'GET'])
 def page_signup():
@@ -601,7 +541,6 @@ def initialize_database():
         connection.commit()
     return redirect(url_for('home_page'))
 
-
 if __name__ == '__main__':
 
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
@@ -616,5 +555,6 @@ if __name__ == '__main__':
         app.config['dsn'] = """user='hknbgryr' password='Yte_gcTO9oEeCFVM7dKv53djw8VnaJwP'
                                host='jumbo.db.elephantsql.com' port=5432 dbname='hknbgryr'"""
 
-    app.run(host='0.0.0.0', port=port, debug=debug)
+    apps = create_app()
+    apps.run(host='0.0.0.0', port=port, debug=debug)
 
